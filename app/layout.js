58878833a81e60fa840e1cddbaf7d3ba6ba07a58
jaffe9 <jaffar.meeran@gmail.com>
2023-09-25 21:14:34 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -27,6 +27,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+        <title>J SAAS UI</title>
+        <meta name='J SAAS UI' content='Description' />
+      </head>
       <body className={inter.className}>
         <SaasProvider linkComponent={Link}>
         <AppShell
